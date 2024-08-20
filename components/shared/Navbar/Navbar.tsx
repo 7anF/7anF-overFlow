7bb3266fdbb61b8bfd1585dc9b525a91ca29b6da
7anF <7anf2005hanafiah@gmail.com>
2024-08-20 13:02:48 -0700

    ends Theme Provider

---
+++ components/shared/Navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Theme from "./Theme";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex gap-1 items-center">
+        <Image
+          src="/assets/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="DevFlow"
+        />
+        <p className="font-spaceGrotesk h2-bold text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="font-bold text-primary-500">Overflow</span>
+        </p>
+      </Link>
+      <div>Search</div>
+      <div className="flex-between gap-5">
+        <Theme />
+        <SignedIn>
+          <UserButton
+            afterSwitchSessionUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#FF7000",
+              },
+            }}
+          />
+        </SignedIn>
+        {/* Mobile Navbar */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
