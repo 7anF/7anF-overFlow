e687b286e75af886835c25bdf109ee5ce69f1de3
7anF <7anf2005hanafiah@gmail.com>
2024-09-06 06:57:17 -0700

    midd

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,19 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-// const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
-const isPublicRoute = createRouteMatcher([
-  "/api/webhooks",
-  "question/:id",
-  "/tags",
-  "/tags/:id",
-  "/profile/:id",
-  "/comunnity",
-  "/jobs",
-]);
-export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) {
-    auth().protect();
-  }
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
