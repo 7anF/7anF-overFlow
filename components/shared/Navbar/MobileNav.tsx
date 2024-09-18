a6636fb1337fcc3bd18970a07c88d623dde764e9
7anF <7anf2005hanafiah@gmail.com>
2024-09-18 12:05:54 -0700

    bug-fixing

--- components/shared/Navbar/MobileNav.tsx
+++ components/shared/Navbar/MobileNav.tsx
@@ -1,43 +1,37 @@
 "use client";
 
-import React from "react";
-
 import {
   Sheet,
-  SheetClose,
   SheetContent,
+  SheetClose,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Image from "next/image";
 import Link from "next/link";
-import { SignedOut, useAuth } from "@clerk/nextjs";
+import { SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import { sidebarLinks } from "@/constants/constants";
 import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants/constants";
 
-export const NavContent = () => {
+const NavContent = () => {
   const pathname = usePathname();
-  const { userId } = useAuth();
-  let isActive = false;
 
   return (
-    <section className="flex flex-col gap-6 pt-16">
+    <section className="flex h-full flex-col gap-6 pt-16">
       {sidebarLinks.map((item) => {
-        isActive =
+        const isActive =
           (pathname.includes(item.route) && item.route.length > 1) ||
           pathname === item.route;
-        if (item.route === "/profile") {
-          if (userId) {
-            item.route = `${item.route}/${userId}`;
-          } else {
-            return null;
-          }
-        }
+
         return (
-          <SheetClose key={item.route}>
+          <SheetClose asChild key={item.route}>
             <Link
               href={item.route}
-              className={`${isActive ? "primary-gradient rounded-lg text-light-900" : "text-dark300_light900"} flex-start gap-4 bg-transparent p-4`}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
             >
               <Image
                 src={item.imgURL}
@@ -60,38 +54,41 @@ export const NavContent = () => {
 const MobileNav = () => {
   return (
     <Sheet>
-      <SheetTrigger>
+      <SheetTrigger asChild>
         <Image
           src="/assets/icons/hamburger.svg"
           width={36}
           height={36}
           alt="Menu"
-          className="invert-colors sm:hidden"
+          className="invert-colors sm:hidden cursor-pointer"
         />
       </SheetTrigger>
+
       <SheetContent
         side="left"
-        className="background-light900_dark200 border-none"
+        className="background-light900_dark200 border-none overflow-y-auto no-scrollbar"
       >
-        <Link href="/" className="flex gap-1 items-center">
+        <Link href="/" className="flex items-center gap-1">
           <Image
             src="/assets/images/site-logo.svg"
             width={23}
             height={23}
             alt="DevFlow"
           />
-          <p className="font-spaceGrotesk h2-bold text-dark100_light900">
-            Dev<span className="text-primary-500">Overflow</span>
+
+          <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
+            Dev <span className="text-primary-500">Overflow</span>
           </p>
         </Link>
-        <div className="flex h-[calc(100vh-80px)] justify-between flex-col overflow-y-auto overflow-scroll">
-          <SheetClose>
+        <div className="no-scrollbar flex grow flex-col justify-between overflow-y-auto">
+          <SheetClose asChild>
             <NavContent />
           </SheetClose>
-
+        </div>
+        <div>
           <SignedOut>
             <div className="flex flex-col gap-3">
-              <SheetClose>
+              <SheetClose asChild>
                 <Link href="/sign-in">
                   <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
                     <span className="primary-text-gradient">Log In</span>
@@ -99,10 +96,10 @@ const MobileNav = () => {
                 </Link>
               </SheetClose>
 
-              <SheetClose>
+              <SheetClose asChild>
                 <Link href="/sign-up">
-                  <Button className="small-medium light-border-2 btn-tertiary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none text-dark400_light900">
-                    Sign up
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                    Sign Up
                   </Button>
                 </Link>
               </SheetClose>
--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -9,20 +9,24 @@ import GlobalSearch from "../search/GlobalSearch";
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link href="/" className="flex gap-1 items-center">
+      <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
           width={23}
           height={23}
           alt="DevFlow"
         />
-        <p className="font-spaceGrotesk h2-bold text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dev<span className="font-bold text-primary-500">Overflow</span>
+
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev <span className="text-primary-500">Overflow</span>
         </p>
       </Link>
+
       <GlobalSearch />
+
       <div className="flex-between gap-5">
         <Theme />
+
         <SignedIn>
           <UserButton
             afterSwitchSessionUrl="/"
@@ -31,14 +35,13 @@ const Navbar = () => {
                 avatarBox: "h-10 w-10",
               },
               variables: {
-                colorPrimary: "#FF7000",
+                colorPrimary: "#ff7000",
               },
             }}
           />
         </SignedIn>
-        <div className="sm:hidden flex-center">
-          <MobileNav />
-        </div>
+
+        <MobileNav />
       </div>
     </nav>
   );
